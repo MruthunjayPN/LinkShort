04f42fc2f516ae4a533062a467d30160deaa173b
Mruthunjay P N <muttu@mpn.dev>
2025-05-02 19:50:20 +0530

    project setup of url shortner

---
+++ LinkShort/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+      <h1 className='text-4xl text-red-400'>db password : 45#gFnGH23XLm#F</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
